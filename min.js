d0b2a247bc02318f27b6082e5b333657ebe2260d
Nikoliy <39348231+TAVOLZHANSKYI@users.noreply.github.com>
2018-10-11 23:58:22 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
+
+// 	document.getElementById('hider').onclick = function() {
+//   	document.getElementById('text').style.display = 'none';
+
+//   	else{
+//   		document.getElementById('text').style.display = 'block';
+//   	}
+// }
+
+
+function change(idName) {
+  if(document.getElementById(idName).style.display=='none') {
+    document.getElementById(idName).style.display = '';
+  } else {
+    document.getElementById(idName).style.display = 'none';
+  }
+  return false;
+}
+
+
+// <div style="display:none" id="test">Hi World! </div>
+// <a href="#" onclick="change('test')">Change</a>
